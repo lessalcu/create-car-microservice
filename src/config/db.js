2f0d75a6a4ae9c918c22b95d1ccaf93c6dad27bf
lessalcu <154089468+lessalcu@users.noreply.github.com>
2025-02-06 21:52:37 -0500

    feat: :bug:

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Lesly1105',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'cardb',
 });
 
